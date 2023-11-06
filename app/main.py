e62a2c26fec6382ad5ed13d03036ff9b853f376a
Sachin B G <Sachin.B@visionetsystems.com>
2023-11-06 20:42:16 +0530

    first-commit

---
+++ app/main.py
@@ -0,0 +1,42 @@
+from typing import Union
+from fastapi import FastAPI, BackgroundTasks
+from loguru import logger
+import sys
+import time
+import random
+import json
+
+
+
+
+logger.add(sys.stdout, colorize=True, format="{time:HH:mm:ss} | {level} | {message}")
+
+app = FastAPI()
+
+
+
+def data_moduler():
+    data = {
+        "Frequence":random.randint(1,4),
+        "StarupBuffer":random.randint(1,4),
+        "Buffer":random.randint(1,4)
+    }
+    return data
+
+
+@app.get("/")
+async def read_root():
+    loop=1
+    while loop<=10000:
+        data = data_moduler()
+        loop = loop+1
+        logger.info("checking the application logs info")
+        logger.debug(data)
+        logger.warning("checking the application logs warning ")
+        logger.error("checking the application logs error")
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
